b7a1452f9d7f791bc9fe1be2829dc9a7f199b0d0
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-27 19:44:14 -0300

    FEAT [Page App]: Import useds pages; import globalStyle file using styled-components; organize elements using BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyle";
+
+import Main from "./pages/Main";
+
+import Login from "./pages/log/Login";
+import Register from "./pages/log/Register";
+
+import Home from "./pages/content/Home";
+import Plan from "./pages/content/Plan";
+import Subscription from "./pages/content/Subscription";
+
 export default function App() {
     return(
-        <></>
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Main>
+                    <Routes>
+                        <Route path="/" element={ <Login /> } />
+                        <Route path="/sign-up" element={ <Register /> } />
+                        <Route path="/home" element={ <Home /> } />
+                        <Route path="/subscriptions" element={ <Subscription /> } />
+                        <Route path="/subscriptions/PLAN_ID" element={ <Plan /> } />
+                    </Routes>
+                </Main>
+            </BrowserRouter>
+        </>
     )
 }
