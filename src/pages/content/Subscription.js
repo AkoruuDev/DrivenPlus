9c7a35faa4bc048b27488982b97b427e4ea284e6
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-27 23:23:47 -0300

    CHORE [Get Plan List]: Create a function for show plans on subscriptions page

--- src/pages/content/Subscription.js
+++ src/pages/content/Subscription.js
@@ -1,5 +1,25 @@
-export default function Subscription() {
+import { useContext, useState } from "react"
+import { planList } from "../../services/API"
+import { AuthContext } from "../../provider/auth"
+
+function Sub() {
     return(
         <></>
+    )
+}
+
+export default function Subscription() {
+    const [plans, setPlans] = useState([])
+    const { user } = useContext(AuthContext);
+    console.log(user);
+
+    planList(user.token)
+        .then(res => console.log(res.data))
+        .catch(() => console.log("Deu Ruim"))
+    return(
+        <>
+            Escolha seu Plano
+            <>{plans?.map(sub => <Sub />)}</>
+        </>
     )
 }
