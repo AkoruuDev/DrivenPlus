12f994fea816a7adf9ca12585ff33540eb78bd78
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-27 21:33:51 -0300

    STYLE [LogIn]: Update style for login page using styled-components on App

--- src/pages/log/Login.js
+++ src/pages/log/Login.js
@@ -1,14 +1,69 @@
+import styled from "styled-components";
 import logo from "../../assets/logo-name.svg";
 
 export default function Login() {
     return(
-        <>
-            <img src={logo} alt="logo"/>
-            <form>
-                <input type="email" name="email" placeholder="E-mail" />
-                <input type="password" name="password" placeholder="Senha" />
-                <button>Entrar</button>
-            </form>
-        </>
+        <Container>
+            <Image src={logo} alt="logo"/>
+            <Form>
+                <Input type="email" name="email" placeholder="E-mail" />
+                <Input type="password" name="password" placeholder="Senha" />
+                <Button>ENTRAR</Button>
+            </Form>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding: 25px 0;
+    background-color: #0e0e13;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`
+
+const Image = styled.img`
+    width: 80vw;
+    margin-bottom: 15vh;
+`
+
+const Form = styled.form`
+    width: 80vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Input = styled.input`
+    width: 100%;
+    height: 52px;
+    border-radius: 8px;
+    margin: 16px 0;
+    padding: 0 14px;
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+    border: 1px solid #7E7E7E;
+    color: #7E7E7E;
+`
+
+const Button = styled.button`
+    width: 100%;
+    height: 52px;
+    margin-top: 24px;
+    border-radius: 8px;
+    background-color: #FF4791;
+    color: #fafafa;
+    font-weight: 700;
+    font-family: 'Roboto', sans-serif;
+`
