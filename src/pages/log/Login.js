8b95eb7a520c65a3118f46ccb8eb72895cd3ffe9
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-27 21:15:02 -0300

    BAS [Login]: Make structure for login page on App

--- src/pages/log/Login.js
+++ src/pages/log/Login.js
@@ -1,5 +1,14 @@
+import logo from "../../assets/logo-name.svg";
+
 export default function Login() {
     return(
-        <></>
+        <>
+            <img src={logo} alt="logo"/>
+            <form>
+                <input type="email" name="email" placeholder="E-mail" />
+                <input type="password" name="password" placeholder="Senha" />
+                <button>Entrar</button>
+            </form>
+        </>
     )
 }
