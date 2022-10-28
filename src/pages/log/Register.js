b5da18adddd88f0a001c87b08435f32ce3c25057
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-27 23:04:18 -0300

    STYLE [Register]: Create structure for Register; Update style for page; create a function for navigate from sign-up page to sign-in

--- src/pages/log/Register.js
+++ src/pages/log/Register.js
@@ -1,5 +1,86 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../../assets/logo-name.svg";
+
 export default function Register() {
+    const navigate = useNavigate();
+
     return(
-        <></>
+        <Container>
+            <Image src={logo} alt="logo"/>
+            <Form>
+                <Input type="text" name="name" placeholder="Nome" />
+                <Input type="text" name="cpf" placeholder="CPF" />
+                <Input type="email" name="email" placeholder="E-mail" />
+                <Input type="password" name="password" placeholder="Senha" />
+                <Button>CADASTRAR</Button>
+            </Form>
+            <Login onClick={() => navigate('/')}>Já possuí uma conta? Entre</Login>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding: 25px 0;
+    background-color: #0e0e13;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`
+
+const Image = styled.img`
+    width: 80vw;
+    margin-bottom: 8vh;
+`
+
+const Form = styled.form`
+    width: 80vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Input = styled.input`
+    width: 100%;
+    height: 52px;
+    border-radius: 8px;
+    margin: 8px 0;
+    padding: 0 14px;
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+    border: 1px solid #7E7E7E;
+    color: #7E7E7E;
+`
+
+const Button = styled.button`
+    width: 100%;
+    height: 52px;
+    margin: 24px 0;
+    border-radius: 8px;
+    background-color: #FF4791;
+    color: #fafafa;
+    font-weight: 700;
+    font-family: 'Roboto', sans-serif;
+`
+
+const Login = styled.p`
+    color: #FFFFFF;
+    font-size: 14px;
+    text-decoration: underline;
+    font-style: italic;
+
+    &:hover {
+        color: #1F87EE;
+    }
+`
