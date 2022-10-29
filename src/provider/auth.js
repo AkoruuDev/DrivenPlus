f5839732c961ae3bd3b74f1d15407a11fab7141f
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-28 22:47:49 -0300

    REFACTOR [Context]: Remove image param no used

--- src/provider/auth.js
+++ src/provider/auth.js
@@ -5,7 +5,6 @@ export const AuthContext = React.createContext({});
 export const AuthProvider = (props) => {
     const [user, setUser] = useState({
         name: '',
-        image: '',
         membership: {},
         token: ''
     })
