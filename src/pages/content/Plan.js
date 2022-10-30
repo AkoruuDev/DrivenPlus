84830b24004b8df2b7675ed0248d28fdbce4a581
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-30 16:19:17 -0300

    FEAT [Plan]: Create a function fot show info plan user choiced from App

--- src/pages/content/Plan.js
+++ src/pages/content/Plan.js
@@ -1,18 +1,24 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { AuthContext } from "../../provider/auth";
 import { showPlan } from "../../services/API";
 
 export default function Plan() {
-    const [plan, setPLan] = useState({});
+    const [plan, setPLan] = useState(undefined);
     const { PLAN_ID } = useParams();
     const { user } = useContext(AuthContext);
 
-    showPlan(PLAN_ID, user.token)
-        .then(res => {
-            setPLan(res.data);
-        })
-        .catch(err => console.log(err))
+    useEffect(() => {
+        showPlan(PLAN_ID, user.token)
+            .then(res => {
+                setPLan(res.data);
+            })
+            .catch(err => console.log(err))
+    }, [])
+    
+    if (plan === undefined) {
+        return(<>Loading...</>)
+    }
 
     return(
         <>
@@ -22,7 +28,7 @@ export default function Plan() {
                     <img src={plan.image} alt="logo"/>
                     <h1>{plan.name}</h1>
                     <h2>Benefícios:</h2>
-                    {plan.perks?.map((perk, i) => <p>{i + 1}. {perk.title}</p>)} {/* Linha com erro */}
+                    {plan.perks.map((perk, i) => <p key={i}>{i + 1}. {perk.title}</p>)} {/* Linha com erro */}
                     <h2>Preço:</h2>
                     <p>R$ {plan.price} cobrados mensalmente</p>
                 </>
