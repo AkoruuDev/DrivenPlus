a15c1fa4e3fe700795054c0443f39ce3ef1da713
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-30 00:54:59 -0300

    CHORE [Show Alert]: Structure info for show in page the error alert

--- src/pages/content/Subscription.js
+++ src/pages/content/Subscription.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react"
 import { planList } from "../../services/API"
 import { AuthContext } from "../../provider/auth"
+import AlertBOX from "../../services/alert"
+import { useNavigate } from "react-router-dom"
 
 function Sub({ sub }) {
     return(
@@ -15,6 +17,23 @@ export default function Subscription() {
     const [plans, setPlans] = useState([])
     const { user } = useContext(AuthContext);
     console.log(user);
+    const navigate = useNavigate();
+
+    // ---------------------------------------------
+    const [show, setShow] = useState(false);
+    const [info, setInfo] = useState('');
+    const [confirmMessage, setConfirmMessage] = useState('');
+    const [action, setAction] = useState();
+    const [doAction, setDoAction] = useState(false);
+    const [secButton, setSecButton] = useState({});
+    // ---------------------------------------------
+
+    function actionConfirm() {
+        if(doAction) {
+            navigate('/');
+            document.location.reload();
+        }
+    }
 
     useEffect(() => {
         planList(user.token)
@@ -22,12 +41,25 @@ export default function Subscription() {
             console.log(res.data);
             setPlans(res.data)
         })
-        .catch(() => console.log("Deu Ruim"))
+        .catch(() => {
+            console.log("Deu Ruim")
+            // ---------------------------------------------
+            setInfo('Ops! Parece que não conseguimos conectar você com os planos...');
+            setConfirmMessage('Tudo bem, vou tentar de novo');
+            setAction(() => setDoAction(true))
+            setSecButton({
+                ...secButton,
+                show: false
+            });
+            setShow(true);
+            // ---------------------------------------------    
+        })
     }, [])
     return(
         <>
             Escolha seu Plano
             <>{plans?.map(sub => <Sub key={sub.id} sub={sub}/>)}</>
+            <AlertBOX show={show} action={action} info={info} confirmMessage={confirmMessage} secButton={secButton} />
         </>
     )
 }
